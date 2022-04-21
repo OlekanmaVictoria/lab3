dca4705ccc22d4be0001878399bb9dd422343a20
OlekanmaVictoria <olekanmavictoria1994@gmail.com>
2022-04-20 18:15:03 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Enter a number;");  
+int number = Convert.ToInt32(Console.ReadLine());
+for (int i = 2; i <= number - 1; i++)  
+            {  
+                if (number % i == 0)  
+                {  
+                    Console.WriteLine( " not a Prime number");
+                    break;
+                }  
+            
+            if (i == number - 1)  
+            {  
+                Console.WriteLine( " Prime number");
+                break;
+            }  
+            
+        }
